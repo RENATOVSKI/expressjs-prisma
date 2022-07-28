cbdeebbc4d577c4ac7e7cc79146e86ec7fd71415
RENATOVSKI <sir.renattowolff@gmail.com>
2022-07-27 21:19:23 -0300

    feat: redis implementation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
+import redis from "./lib/cache";
+
 
 const prisma = new PrismaClient();
 
@@ -7,16 +9,29 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.raw({ type: "application/vnd.custom-type" }));
-app.use(express.text({ type: "text/html" }));
+
+const todosKey = "cachedTodos"
 
 app.get("/todos", async (req, res) => {
-  const todos = await prisma.todo.findMany({
-    orderBy: { createdAt: "desc" },
-  });
+  try {
+    const cachedTodos = await redis.get(todosKey)
 
-  res.json(todos);
-});
+    if (cachedTodos) {
+      return res.json(JSON.parse(cachedTodos));
+    }
+
+    const todos = await prisma.todo.findMany({
+      orderBy: { createdAt: "desc" },
+    });
+
+    await redis.set(todosKey, JSON.stringify(todos))
+    return res.json(todos)
+
+  } catch (error: any) {
+    console.error(error)
+    return res.status(500).json({ error: error.message })
+  }
+})
 
 app.post("/todos", async (req, res) => {
   const todo = await prisma.todo.create({
@@ -26,49 +41,8 @@ app.post("/todos", async (req, res) => {
       text: req.body.text ?? "Empty todo",
     },
   });
-
+  redis.del(todosKey)
   return res.json(todo);
-});
-
-app.get("/todos/:id", async (req, res) => {
-  const id = req.params.id;
-  const todo = await prisma.todo.findUnique({
-    where: { id },
-  });
-
-  return res.json(todo);
-});
-
-app.put("/todos/:id", async (req, res) => {
-  const id = req.params.id;
-  const todo = await prisma.todo.update({
-    where: { id },
-    data: req.body,
-  });
-
-  return res.json(todo);
-});
-
-app.delete("/todos/:id", async (req, res) => {
-  const id = req.params.id;
-  await prisma.todo.delete({
-    where: { id },
-  });
-
-  return res.send({ status: "ok" });
-});
-
-app.get("/", async (req, res) => {
-  res.send(
-    `
-  <h1>Todo REST API</h1>
-  <h2>Available Routes</h2>
-  <pre>
-    GET, POST /todos
-    GET, PUT, DELETE /todos/:id
-  </pre>
-  `.trim(),
-  );
 });
 
 app.listen(port, () => {
