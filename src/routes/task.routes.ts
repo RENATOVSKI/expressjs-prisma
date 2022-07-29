f32f8932d4f96539f3e3370fbea80f100964dff8
RENATOVSKI <sir.renattowolff@gmail.com>
2022-07-29 00:41:53 -0300

    feat: controller, services, routes, findAll, findId, create, task

--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -2,7 +2,9 @@ import { Express } from "express";
 import * as controller from "../controllers/task.controller";
 
 const taskRoutes = (app: Express) => {
-  app.route("/task").get(controller.findAllTasks);
+  app.route("/tasks").get(controller.findAllTasks);
+  app.route("/tasks/:id").get(controller.findTaskById);
+  app.route("/tasks").post(controller.createTask);
 };
 
 export default taskRoutes;
--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -1,11 +1,41 @@
 import { prisma } from "../database/prismaClient";
+import cache from "../lib/cache";
 import Cache, { cachedTaskKey } from "../lib/cache";
 
 export const findAllTasks = async () => {
+  const cachedTask = await Cache.get(cachedTaskKey);
+  if (cachedTask) return cachedTask;
+
   const tasks = await prisma.task.findMany({
     orderBy: { createdAt: "desc" },
   });
 
   Cache.set(cachedTaskKey, tasks, 60 * 10);
   return tasks;
+}
+
+export const findTaskById = async (id: string) => {
+  const cachedTask = await Cache.get(`${cachedTaskKey}-${id}`);
+  if (cachedTask) return cachedTask;
+  const task = await prisma.task.findFirst({
+    where: { id },
+  });
+
+  if (!task) throw new Error("Task not found");
+
+  Cache.set(`${cachedTaskKey}-${id}`, task, 60 * 10);
+  return task;
+}
+
+export const createTask = async (title: string, text: string) => {
+  const create = await prisma.task.create({
+    data: {
+      title,
+      text,
+      completed: false,
+    },
+  });
+  Cache.delPrefix(cachedTaskKey);
+  return create;
+
 }
